46ebab154910e335af472155e13d07ff951a69c1
Dario Tedeschi <dario.tedeschi86@hotmail.it>
2021-03-30 12:14:27 +0200

    ok

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 var app = new Vue ({
     el: "#app",
-    lstMail: {
-        utnMail: this.mail,
+    data: {
+        utnMail:[]
     },
-
+    
     mounted() {
         const self = this
         for (var i = 0; i < 10; i++){
-           axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then(function (mail) {
-                console.log(mail.data.response)
-                this.utnMail = mail.data.respond
+           axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((result) => {
+                this.utnMail.push(result.data.response);
             })  
         }
     }
